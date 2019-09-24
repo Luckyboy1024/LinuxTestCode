91d1665666971d817893f47f3ea742b032e5978a
Luckyboy1024 <2819549538@qq.com>
2019-09-24 12:27:52 +0800

    提交2019年09月24日的测试代码

---
+++ test_1/test.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+void *Mymemcpy(void *dest, const void *src, int count)
+{
+	char *p = (char*)dest;
+	while (count--)
+	{
+		*p = *(char*)src;		//类型强转，char大小一个字节
+		p++;
+		(char*)src++;
+	}
+	return dest;
+}
+
+int main()
+{
+	int arr1[] = { 1, 2, 3, 4, 5, 6, 7, 8 };
+	int arr2[] = { 1, 2, 3, 4, 5, 6, 7, 8 };
+	memcpy(arr1 + 2, arr1, 16);
+	Mymemcpy(arr2+2, arr2, 16);		
+	return 0;
+}
